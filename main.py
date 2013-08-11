a258d40700feb40556ccba2e9bd71b3b53a8fb6f
krist-jin <krist333@126.com>
2013-08-11 15:16:26 -0700

    v2.1

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import collections
 from collections import Counter
 class Technology(object):    
     def __init__(self, label, color):
-        """Each technology should be constructed with
-        a text label and a color for display purposes"""
         self.label=label
         self.color=color
-
-
+        
+    def __str__(self):
+        return(str(self.label)+"&"+str(self.color))
+    
+    
 
 class User(object):
     def __init__(self,user_id):
@@ -19,151 +20,117 @@ class User(object):
         self.friends_list=[]
 
     def __lt__(self, other):
-        """this is a built in comparison function, so that
-        user1 < user2 always returns False.  You may not need
-        this method at all, but it can sometimes be handy
-        for sorting lists that contain users without throwing
-        errors"""
-        return False
+        if self.prv < other.prv:
+            return True
+        else:
+            return False
     
     def get_friends(self):
-        """returns a list of the user's friends,
-        which are also users"""
         return self.friends_list
-    
 
     def is_friend(self, other):
-        """returns True if this User and other are friends.
-            returns False otherwise"""
-        if other.user_id in self.friends_list:
+        if other in self.friends_list:
             return True
         else:
             False
-    
 
     def get_id(self):
-        """Returns this User's id"""
         return self.user_id
-    
 
     def get_tech(self):
-        """Returns the Technology used by the User, or None if
-        the User has no Technlology"""
         return self.tech
 
     def __repr__(self):
         return (str(self.user_id)+": "+str(self.tech))
 
 
-class Graph(object):
 
+class Graph(object):
     def __init__(self, population):
-        """create a new graph with population users and no connections.
-        no users in the new graph should have any technology"""
         self.population=population
         self.users_list=[]
         for i in range(self.population):
             self.users_list.append(User(i))
-    
-
 
     def circle_connect(self, n):
-        """connect each user i to the next n users, that is
-        users (i+1)%population, (i+2)%population,...,
-        (i+n)%population."""
         i=0
-        while  i < self.population:
-##            if i < n:
-##                z=0
-##                while z+i<n:
-##                    self.users_list[i].friends_list.append(self.users_list[self.population-n+i+z].user_id)
-##                    z+=1
+        while i < self.population:
             j=1
             while j<=n:
-                self.users_list[i%self.population].friends_list.append(self.users_list[(i%self.population+j)%self.population])
-                self.users_list[(i%self.population+j)%self.population].friends_list.append(self.users_list[i%self.population])
+                self.users_list[i].friends_list.append(self.users_list[(i+j)%self.population])
+                self.users_list[(i+j)%self.population].friends_list.append(self.users_list[i])
                 j+=1
-##            print(self.users_list[i%self.population].friends_list)               #### USED FOR TEST
             i+=1
-       
-
-                
-        
             
     def random_connections(self, num_connections):
-        """add num_connections new connections randomly to the graph"""
         while num_connections > 0:
             a=random.choice(self.users_list)
             b=random.choice(self.users_list)
             if a==b:
                 continue
+            elif a.is_friend(b) or b.is_friend(a):
+                continue 
             else :
                 a.friends_list.append(b)
                 b.friends_list.append(a)
                 num_connections-=1
 
-
-    def is_connected(self):
+    def is_connected(self, user, limit):
         """returns True if there is a path from every graph User to
         every other user"""
         ## judge whether the undirected graph is connected
-        connected_user=set()
-        for i in range(self.population):
-            for j in range(len(self.users_list[i].friends_list)):
-                connected_user.add(self.users_list[i].friends_list[j])
-        if len(connected_user)==self.population:
-            return True
+        if limit < 0:
+            return
         else:
-            return False
-        
-    
-
-    def time_step(self):
-        """move the simulation forward by one time period.
-        Each user adopts the most popular technology among
-        themselves and their friends.  If there are multiple
-        technologies that are tied for most popular, the user
-        selects one at random."""
-        # To find the most common tech in a subset of users,
-        # you cannot build a frequency table using a
-        # dictionary since Technologies are mutable objects.
-        # If you want to follow a similar strategy, you can
-        # import the collections package and use a Counter
-        # object, which works like a (slow) dictionary for
-        # mutable objects.  However, there are many other
-        # strategies that you could use to do the same thing.
-        
-####        while any(self.users_list[i].label == None for i in range(self.population)):
-        for i in range(self.population):
-            for j in range(len(self.users_list[i].get_friends())):
-                self.users_list[i].tech_list.append(self.users_list[self.users_list[i].get_friends()[j].get_id()].get_tech())
-####                for n,s in enumerate(self.users_list[i].tech_list):
-####                    if s==None:
-####                        self.users_list[i].tech_list[n]=0       
-####            print(self.users_list[i].tech_list)
-            if all(x == None for x in self.users_list[i].tech_list):
-                self.users_list[i].temp_tech=self.users_list[i].tech
-####                print(self.users_list[0].temp_tech)
-####            elif((self.users_list[i].tech_list).count(k for k in self.users_list[i].tech_list)==1):
-####                self.users_list[i].temp_tech=self.users_list[i].tech
-            else:
-                self.users_list[i].tech_list=list(filter((None).__ne__, self.users_list[i].tech_list))
-                counter=collections.Counter(self.users_list[i].tech_list)
-                if (counter.most_common(1)[0][1]==1 and self.users_list[i].tech !=None):
-                    self.users_list[i].temp_tech=self.users_list[i].tech
-                else:
-####                    print((self.users_list[i].tech_list).count(k))
-                    self.users_list[i].temp_tech=counter.most_common(1)[0][0]
-####                    self.users_list[i].temp_tech=max(k for k in self.users_list[i].tech_list if (self.users_list[i].tech_list).count(k)>1)           
-        for i in range(self.population):
-            self.users_list[i].tech=self.users_list[i].temp_tech
+            for friend in user.get_friends():
+                limit-=1
+                self.tmp_node_set.add(friend)
+                self.is_connected(friend,limit)    
+        return (len(self.tmp_node_set)), len(self.get_users())
+#         self.tmp_node_set=set()
+#         for a in user.get_friends():
+#             for b in a.get_friends():
+#                 for c in b.get_friends():
+#                     for d in c.get_friends():
+#                         for e in d.get_friends():
+#                             for f in e.get_friends():
+#                                 for g in f.get_friends():
+#                                     for h in g.get_friends():
+#                                         for i in h.get_friends():
+#                                             for j in i.get_friends():
+#                                                 for k in j.get_friends():
+#                                                     for l in k.get_friends():
+#                                                         for m in l.get_friends():
+#                                                             self.tmp_node_set.add(m)
+#         return (len(self.tmp_node_set)), len(self.get_users())
             
-#             ##USED FOR TEST(OUTPUT)
-#         for i in range(self.population):
-#             print(i,self.users_list[i].tech)
-#             
         
-        
+    def time_step(self):
+        for user in self.users_list:
+            for friend in user.get_friends():
+                user.tech_list.append(friend.get_tech())
+            user.temp_tech = self.get_next_tech(user)
+        for user in self.users_list:
+            user.tech=user.temp_tech
+    
+    def get_next_tech(self, user):
+        friends_tech_list = []
+        for friend in user.get_friends():
+            friends_tech_list.append(friend.get_tech())
+        friends_tech_list.append(user.get_tech())
+        friends_tech_list=list(filter((None).__ne__, friends_tech_list))
+        c = collections.Counter(friends_tech_list)
+        tmp_key_list=[]
+        for key in c:
+            if c[key] == max(c.values()):
+                tmp_key_list.append(key)
+        if len(tmp_key_list) == 0:
+            result = None
+        else:
+            result = random.choice(tmp_key_list)
+#         print("result"+str(result))
+        return result
+            
     def get_users(self):
         """returns a list containing the users in the graph, in
         order of their IDs"""
@@ -179,7 +146,6 @@ class Graph(object):
         return result
 
 
-
     
 class GraphAnalyzer(object):
     def __init__(self, graph, my_tech):
@@ -191,13 +157,11 @@ class GraphAnalyzer(object):
         self.first_time_flag = True
         self.plan_list = []
         self.first_adopter_list = []
-##        self.counter = 0
 
     def choose_user(self):
         """returns a user that does not currently have
         a technology, to serve as a first-adopter for 
         this analyzer's technology"""
-#         print("choose user is called...")
         if self.first_time_flag == True:
             self.pagerank()
             self.plan_list = self.get_untouched(self.user_list)
@@ -211,7 +175,6 @@ class GraphAnalyzer(object):
             self.plan_list = self.get_untouched(self.plan_list)
             best = self.get_best_from_plan()
             self.first_adopter_list.append(best)
-            
             return best
 
     def pagerank(self, damping_factor=0.85, max_iterations=100, min_delta=0.00001):
@@ -221,9 +184,7 @@ class GraphAnalyzer(object):
         # value for nodes without inbound links
         min_value = (1.0-damping_factor)/graph_size 
          
-        # itialize the page rank dict with 1/N for all nodes
-        #pagerank = dict.fromkeys(nodes, 1.0/graph_size)
-##        pagerank = dict.fromkeys(nodes, 1.0)
+        # itialize the page rank for all nodes
         for user in self.user_list:
             user.prv = 1.0
              
@@ -236,7 +197,6 @@ class GraphAnalyzer(object):
                     tmp_prv += damping_factor * friend.prv / len(friend.get_friends())
                 diff += abs(user.prv - tmp_prv)
                 user.prv = tmp_prv
-             
      
             #stop if PageRank has converged
             if diff < min_delta:
@@ -246,8 +206,6 @@ class GraphAnalyzer(object):
 #         for user in self.user_list:
 #             print(str(user.get_id())+"\t"+str(user.prv))
 
-            
-     
     def get_untouched(self, user_list):
         untouched_list=[]
         for user in user_list:
@@ -275,16 +233,14 @@ class GraphAnalyzer(object):
             plan_list = list(set(plan_list) | set(user.get_friends()))
         return plan_list
     
+    
+    
+    
 
 ##TEST CODE
 gp=Graph(500)
 gp.circle_connect(3)
 gp.random_connections(50)
-
-# gp.users_list[0].tech="fuji"
-# gp.users_list[3].tech="canon"
-# gp.users_list[7].tech="nikon"
-
 my_tech = Technology("krist","green")
 team_list=[]
 for i in range(12):
@@ -313,14 +269,3 @@ for user in gp.users_list:
     result.append(str(user.get_tech().label))
 print("final result: ")
 print(Counter(result).most_common(13))
-
-        
-        
-    
-
-
-# ga.pagerank()
-# print(gp)
-# print("First choose: " + str(ga.choose_user().get_id()))
-# print("Second choose: " + str(ga.choose_user().get_id()))
-# print("Third choose: " + str(ga.choose_user().get_id()))
